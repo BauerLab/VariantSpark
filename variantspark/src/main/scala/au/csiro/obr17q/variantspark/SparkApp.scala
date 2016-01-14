9e98bdca87a8b6f952081d64fd3f01311204e23e
O'Brien, Aidan (CCI, Acton) <Aidan.O'Brien@csiro.au>
2016-01-14 14:43:25 +1100

    setting for running locally

--- variantspark/src/main/scala/au/csiro/obr17q/variantspark/SparkApp.scala
+++ variantspark/src/main/scala/au/csiro/obr17q/variantspark/SparkApp.scala
@@ -6,15 +6,16 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 trait SparkApp {
   def defaultMasterUrl = "yarn-client"
-  //Logger.getLogger("org").setLevel(Level.OFF)
-  //Logger.getLogger("akka").setLevel(Level.OFF)
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
   
   lazy val conf = new SparkConf()
-  //.set("spark.shuffle.memoryFraction", "0.2")
-  //.set("spark.storage.memoryFraction", "0.4")
-  //.set("spark.yarn.executor.memoryOverhead", "2048")
-  .set("spark.driver.maxResultSize","2048")
-  //.set("spark.default.parallelism", "256")
+    //.setMaster("local")
+    //.set("spark.shuffle.memoryFraction", "0.2")
+    //.set("spark.storage.memoryFraction", "0.4")
+    //.set("spark.yarn.executor.memoryOverhead", "2048")
+    .set("spark.driver.maxResultSize","2048")
+    //.set("spark.default.parallelism", "256")
   lazy val masterUrl =  if (System.getenv("MASTER") != null) System.getenv("MASTER") else "yarn-client"
   lazy val sc = { println("MASTER: " + masterUrl) ; new SparkContext(conf)}
   lazy val sqlContext = new org.apache.spark.sql.SQLContext(sc)
