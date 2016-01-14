7f10d0382ef6847883fdc1795aae2424e0480ed4
O'Brien, Aidan (CCI, Acton) <Aidan.O'Brien@csiro.au>
2016-01-14 14:41:14 +1100

    Getting up to date

--- vcf-clusterer/src/au/csiro/GenotypeWritable.java
+++ vcf-clusterer/src/au/csiro/GenotypeWritable.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.WritableComparable;
 
 /**
