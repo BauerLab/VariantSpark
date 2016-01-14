b381df8e7f1aa4ca2de75002eed632aa36b5d3e2
O'Brien, Aidan (CCI, Acton) <Aidan.O'Brien@csiro.au>
2016-01-14 14:44:55 +1100

    Refactoring the forest

--- variantspark/src/main/scala/au/csiro/obr17q/variantspark/VcfForest.scala
+++ variantspark/src/main/scala/au/csiro/obr17q/variantspark/VcfForest.scala
@@ -1,121 +1,80 @@
 package au.csiro.obr17q.variantspark
 
-import org.apache.spark.mllib.linalg._
-import org.apache.spark.mllib.regression.LabeledPoint
+import au.csiro.obr17q.variantspark.model.CsvParser
 import org.apache.spark.ml.classification.RandomForestClassifier
 import org.apache.spark.ml.classification.RandomForestClassificationModel
+import org.apache.spark.ml.regression.RandomForestRegressionModel
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.PipelineModel
+import org.apache.spark.ml.regression.RandomForestRegressor
 import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import scala.io.Source
 import org.apache.spark.sql.{DataFrame, Row}
 import org.apache.spark.ml.feature.{VectorIndexer, StringIndexer}
 import org.apache.spark.rdd.RDD
+import java.io._
 /**
  * @author obr17q
  */
 object VcfForest extends SparkApp {
   conf.setAppName("VCF foresting")
 
-  case class Record(individual: String, sampleType: String, bmi: Double, preLabel: String, features: Vector)
+  def main(args: Array[String]) {
 
-  
-  def main(args:Array[String]) {
-    
-    //val args = Array("data/smALL.vcf","5","5","100","auto","0.5")
-    
-    val seed = 3262362
-    val VcfFiles = args(0)
-    val NumTrees = args(1).toInt
+    val defaults = Array("/Users/obr17q/Desktop/coadread_mutations_bmi_filtered.csv","5","5","10","auto","0")
+
+
+    //val seed = 3262362
+    val VcfFiles = if (args.length > 0) args(0) else defaults(0)
+    val NumTrees = if (args.length > 1) args(1).toInt else defaults(1).toInt
     //val maxDepth = args(2).toInt
 
-    val FeatureSubsetStrategy = args(4)
-    val VariantCutoff = args(5).toInt
+    val FeatureSubsetStrategy = if (args.length > 4) args(4) else defaults(4)
+    val VariantCutoff = if (args.length > 5) args(5).toInt else defaults(5).toInt
 
 
-    val numFolds = 10
+    // "f1", "precision", "recall", "weightedPrecision", "weightedRecall"
+    val metricName = "f1"
+    val numFolds = 8
+    val labelName = "bmi_cat"
     val maxDepth = Array(5, 10)
     //val maxDepth = Array(5)
-    val maxBins = Array(2, 10, 20)
+    val maxBins = Array(20, 40)
     //val maxBins = Array(10)
 
-    /**
-     * TCGA settings
-     * 01 - Individual ID
-     * 06 - Sex
-     * 46 - Weight
-     * 47 - Height
-     */
-    val PopFiles = Source.fromFile("data/nationwidechildrens.org_clinical_patient_coad.txt").getLines()
-    val IndividualMeta = sc.parallelize(new MetaDataParser(PopFiles, 3, '\t', "[Not Available]", IndividualIdCol = 1, PopulationCol = 2 )(WeightCol = 46, HeightCol = 47, SexCol = 6).returnBmiMap())
 
-    
     /**
-     * TCGA settings
-     * 00 - Individual
-     * 01 - Population
-     * 02 - Super Population
-     * 03 - Gender
-     */
+      * TCGA settings
+      * 01 - Individual ID
+      * 06 - Sex
+      * 46 - Weight
+      * 47 - Height
+      */
+    //val PopFiles = Source.fromFile("data/nationwidechildrens.org_clinical_patient_coad.txt").getLines()
+    //val IndividualMeta : RDD[IndividualMap] = sc.parallelize(new MetaDataParser(PopFiles, 3, '\t', "[Not Available]", IndividualIdCol = 1, PopulationCol = 2)(WeightCol = 46, HeightCol = 47, SexCol = 6).returnBmiMap())
+
+
+    /**
+      * TCGA settings
+      * 00 - Individual
+      * 01 - Population
+      * 02 - Super Population
+      * 03 - Gender
+      */
     //val PopFiles = Source.fromFile("data/ALL.panel").getLines()
-    //val IndividualMeta = sc.parallelize(new MetaDataParser(PopFiles, HeaderLines = 1, '\t', "", 0, 2 )(SexCol = 3).returnMap())
+    //val IndividualMeta : RDD[IndividualMap] = sc.parallelize(new MetaDataParser(PopFiles, HeaderLines = 1, '\t', "", 0, 2 )(SexCol = 3).returnMap())
 
 
-    val vcfObject = new VcfParser(VcfFiles, VariantCutoff, sc)
+    //val modObject = new VcfParser(VcfFiles, VariantCutoff, IndividualMeta, sc)
+    val modObject = new CsvParser(VcfFiles, VariantCutoff, sc)
+    val data = modObject.data
 
-    val NoOfAlleles = vcfObject.variantCount
+    println(data.count)
 
-    val FilteredAlleles = vcfObject.individualTuples
-
-    val IndividualVariants = FilteredAlleles
-      .groupByKey //group by individual ID, i.e. get RDD of individuals
-      .map(p => (p._1.split('_')(0).substring(0,12), (p._1.split('_')(1), p._2))) // Split the TCGA key to get ID & type
-      .filter(_._2._1 == "NORMAL")
-    //.map(p => (p._1, (p._1, p._2))) // 1000 data
-      .join(IndividualMeta.map(_.toBMI)) //filter out individuals lacking required data
-      .map(h =>
-      /*
-      (h._1, h._2._1._1, h._2._2, LabeledPoint(if (h._2._2 =="EUR") 0
-                  else if (h._2._2 =="AFR") 1 else if (h._2._2 =="AMR") 2
-                  else if (h._2._2 =="EAS") 3
-                  else -1, Vectors.sparse(NoOfAlleles, h._2._1._2.to[Seq] ))) // Binary labels
-
-      (h._1, h._2._1._1, h._2._2, LabeledPoint(if (h._2._2 =="GBR") 0
-                  else if (h._2._2 =="ASW") 1 else if (h._2._2 =="CHB") 2
-                  else -1, Vectors.sparse(NoOfAlleles, h._2._1._2.to[Seq] ))) // Binary labels
-      */            
-                  
-      (h._1, h._2._1._1, h._2._2, LabeledPoint(if (h._2._2 > 40) 2 else if (h._2._2 > 30) 1.0 else 0.0, Vectors.sparse(NoOfAlleles, h._2._1._2.to[Seq] ) ))
-          
-
-          //if (h._2._2 == 1) LabeledPoint(h._2._2, Vectors.sparse(NoOfAlleles, h._2._1._2._2.to[Seq] ))
-          //else LabeledPoint(h._2._2, Vectors.sparse(NoOfAlleles, Seq((1,1.0), (2,1.0)) ))
-          
-      
-      )  // Continuous labels
-    .cache
-    //.map(h => (h._1, h._2, h._3, h._4.label)).collect().foreach(println)
-
-    //val ReducedFeatureSelectedSet: RDD[(String, String, Double, LabeledPoint)] = sc
-    //.objectFile("reducedset", 100)
-    
-    //val DataA = ReducedFeatureSelectedSet.map(p => (p._1, p._2, p._3, LabeledPoint(p._3, p._4.features)))
-
-
-
-    val data = sqlContext.createDataFrame(IndividualVariants.map(p => Record(p._1, p._2, p._3, p._4.label.toString, p._4.features))).toDF
-
-    val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
-
-    val Array(trainingData2, testData2) = data.randomSplit(Array(0.7, 0.3))
-
-    val Array(trainingData3, testData3) = data.randomSplit(Array(0.7, 0.3))
-
-    val labelIndexer = new StringIndexer()
-      .setInputCol("preLabel")
-      .setOutputCol("label")
-      .fit(data)
+    //val labelIndexer = new StringIndexer()
+    //  .setInputCol(labelName)
+    //  .setOutputCol("label")
+    //  .fit(data)
 
     //val featureIndexer = new VectorIndexer()
     //  .setInputCol("preFeatures")
@@ -123,24 +82,33 @@ object VcfForest extends SparkApp {
     //  .setMaxCategories(100)
     //  .fit(data)
 
-    val rf = new RandomForestClassifier()
+    val rfClassifier = new RandomForestClassifier()
       .setLabelCol("label")
       .setFeaturesCol("features")
       .setNumTrees(NumTrees)
       .setFeatureSubsetStrategy(FeatureSubsetStrategy)
 
+    val rfRegressor = new RandomForestRegressor()
+      .setLabelCol("bmi")
+      .setFeaturesCol("features")
+      .setNumTrees(NumTrees)
+      .setFeatureSubsetStrategy(FeatureSubsetStrategy)
+
+
+
+
     val pipeline = new Pipeline()
-      .setStages(Array(labelIndexer, rf))
+      //.setStages(Array(labelIndexer, rf))
+      .setStages(Array(rfRegressor))
 
     val evaluator = new MulticlassClassificationEvaluator()
-      .setLabelCol("label")
+      .setLabelCol("bmi")
       .setPredictionCol("prediction")
-      // "f1", "precision", "recall", "weightedPrecision", "weightedRecall"
-      .setMetricName("f1")
+      .setMetricName(metricName)
 
     val paramGrid = new ParamGridBuilder()
-      .addGrid(rf.maxDepth, maxDepth)
-      .addGrid(rf.maxBins, maxBins)
+      .addGrid(rfRegressor.maxDepth, maxDepth)
+      .addGrid(rfRegressor.maxBins, maxBins)
       .build
 
     val cv = new CrossValidator()
@@ -150,11 +118,6 @@ object VcfForest extends SparkApp {
       .setNumFolds(numFolds)
 
     //val model = pipeline.fit(trainingData)
-
-
-
-
-
 
 
     /**
@@ -171,52 +134,110 @@ object VcfForest extends SparkApp {
       }
     */
 
+    val AlleleTuples = modObject.alleleTuples
 
-
-    val AlleleTuples = vcfObject.alleleTuples
-
-
-    def modelFit(df : DataFrame): RDD[(Int, (String, Double))] = {
+    def modelFit(df: DataFrame): RDD[(Int, (String, Double))] = {
       val cvModel = cv.fit(df)
 
       val forestModel = cvModel
         .bestModel
         .asInstanceOf[PipelineModel]
-        .stages(1)
-        .asInstanceOf[RandomForestClassificationModel]
+        .stages(0 )
+        .asInstanceOf[RandomForestRegressionModel]
 
       val importantFeatures = forestModel.featureImportances
 
       AlleleTuples
-        .filter(p => importantFeatures(p._1) > 0.002 )
-        .map(p => (p._1, (p._2, importantFeatures(p._1))) )
+        .filter(p => importantFeatures(p._2) > 0.002)
+        .map(p => (p._2, (p._1, importantFeatures(p._2))))
     }
 
 
 
+    //val Array(trainingData0, testData0) = data.randomSplit(Array(0.8, 0.2))
+    //val Array(trainingData1, testData1) = data.randomSplit(Array(0.8, 0.2))
+    //val Array(trainingData2, testData2) = data.randomSplit(Array(0.8, 0.2))
+    //val Array(trainingData3, testData3) = data.randomSplit(Array(0.8, 0.2))
+    //val Array(trainingData4, testData4) = data.randomSplit(Array(0.8, 0.2))
+    //val Array(trainingData5, testData5) = data.randomSplit(Array(0.8, 0.2))
+    //val Array(trainingData6, testData6) = data.randomSplit(Array(0.8, 0.2))
+    //val Array(trainingData7, testData7) = data.randomSplit(Array(0.8, 0.2))
+    //val Array(trainingData8, testData8) = data.randomSplit(Array(0.8, 0.2))
+    //val Array(trainingData9, testData9) = data.randomSplit(Array(0.8, 0.2))
 
+    val filteredAlleleTuples0 = modelFit(data)
+    val filteredAlleleTuples1 = modelFit(data)
+    val filteredAlleleTuples2 = modelFit(data)
+    val filteredAlleleTuples3 = modelFit(data)
+    val filteredAlleleTuples4 = modelFit(data)
+    /*val filteredAlleleTuples5 = modelFit(data)
+    val filteredAlleleTuples6 = modelFit(data)
+    val filteredAlleleTuples7 = modelFit(data)
+    val filteredAlleleTuples8 = modelFit(data)
+    val filteredAlleleTuples9 = modelFit(data)
+    val filteredAlleleTuples10 = modelFit(data)
+    val filteredAlleleTuples11 = modelFit(data)
+    val filteredAlleleTuples12 = modelFit(data)
+    val filteredAlleleTuples13 = modelFit(data)
+    val filteredAlleleTuples14 = modelFit(data)
+    val filteredAlleleTuples15 = modelFit(data)
+    val filteredAlleleTuples16 = modelFit(data)
+    val filteredAlleleTuples17 = modelFit(data)
+    val filteredAlleleTuples18 = modelFit(data)
+    val filteredAlleleTuples19 = modelFit(data)
+    val filteredAlleleTuples20 = modelFit(data)
+    val filteredAlleleTuples21 = modelFit(data)
+    val filteredAlleleTuples22 = modelFit(data)
+    val filteredAlleleTuples23 = modelFit(data)
+    val filteredAlleleTuples24 = modelFit(data)
+    val filteredAlleleTuples25 = modelFit(data)
+    val filteredAlleleTuples26 = modelFit(data)
+    val filteredAlleleTuples27 = modelFit(data)
+    val filteredAlleleTuples28 = modelFit(data)
+    val filteredAlleleTuples29 = modelFit(data)
+    val filteredAlleleTuples30 = modelFit(data)
+    val filteredAlleleTuples31 = modelFit(data)
+    val filteredAlleleTuples32 = modelFit(data)
+    val filteredAlleleTuples33 = modelFit(data)
+    val filteredAlleleTuples34 = modelFit(data)
+    val filteredAlleleTuples35 = modelFit(data)
+    val filteredAlleleTuples36 = modelFit(data)
+    val filteredAlleleTuples37 = modelFit(data)
+    val filteredAlleleTuples38 = modelFit(data)
+    val filteredAlleleTuples39 = modelFit(data)
+    val filteredAlleleTuples40 = modelFit(data)
+    val filteredAlleleTuples41 = modelFit(data)
+    val filteredAlleleTuples42 = modelFit(data)
+    val filteredAlleleTuples43 = modelFit(data)
+    val filteredAlleleTuples44 = modelFit(data)
+    val filteredAlleleTuples45 = modelFit(data)
+    val filteredAlleleTuples46 = modelFit(data)
+    val filteredAlleleTuples47 = modelFit(data)
+    val filteredAlleleTuples48 = modelFit(data)
+    val filteredAlleleTuples49 = modelFit(data)*/
 
-    val filteredAlleleTuples1 = modelFit(trainingData)
-    val filteredAlleleTuples2 = modelFit(trainingData2)
-    val filteredAlleleTuples3 = modelFit(trainingData3)
-
-
-
+    //val filteredAlleleTuples9 = modelFit(trainingData9)
 
     //(19:16422392, 0.002136421712119211) (19:16422392, 0.004436421712119211)
     // (2, 19:16422392, 0.006)
-    val allOfThem = ( filteredAlleleTuples1 union filteredAlleleTuples2 union filteredAlleleTuples3 )
-    .aggregateByKey((0, "", 0.0))((acc, value) =>  (acc._1 + 1, value._1, acc._3 + value._2), (acc1, acc2) => (acc1._1 + acc2._1, acc1._2, acc1._3 + acc2._3))
+    val allOfThem = (filteredAlleleTuples0 union filteredAlleleTuples1 union filteredAlleleTuples2 union filteredAlleleTuples3 union filteredAlleleTuples4) /* union
+                     filteredAlleleTuples5 union filteredAlleleTuples6 union filteredAlleleTuples7 union filteredAlleleTuples8 union filteredAlleleTuples9 union
+                     filteredAlleleTuples10 union filteredAlleleTuples11 union filteredAlleleTuples12 union filteredAlleleTuples13 union filteredAlleleTuples14 union
+                     filteredAlleleTuples15 union filteredAlleleTuples16 union filteredAlleleTuples17 union filteredAlleleTuples18 union filteredAlleleTuples19 union
+                     filteredAlleleTuples20 union filteredAlleleTuples21 union filteredAlleleTuples22 union filteredAlleleTuples23 union filteredAlleleTuples24 union
+                     filteredAlleleTuples25 union filteredAlleleTuples26 union filteredAlleleTuples27 union filteredAlleleTuples28 union filteredAlleleTuples29 union
+                     filteredAlleleTuples30 union filteredAlleleTuples31 union filteredAlleleTuples32 union filteredAlleleTuples33 union filteredAlleleTuples34 union
+                     filteredAlleleTuples35 union filteredAlleleTuples36 union filteredAlleleTuples37 union filteredAlleleTuples38 union filteredAlleleTuples39 union
+                     filteredAlleleTuples40 union filteredAlleleTuples41 union filteredAlleleTuples42 union filteredAlleleTuples43 union filteredAlleleTuples44 union
+                     filteredAlleleTuples45 union filteredAlleleTuples46 union filteredAlleleTuples47 union filteredAlleleTuples48 union filteredAlleleTuples49 )*/
+    //val allOfThem = (filteredAlleleTuples0  union filteredAlleleTuples1)
+      .aggregateByKey((0, "", 0.0))((acc, value) => (acc._1 + 1, value._1, acc._3 + value._2), (acc1, acc2) => (acc1._1 + acc2._1, acc1._2, acc1._3 + acc2._3))
 
 
 
 
 
     allOfThem.map(p => (p._1, p._2._2, p._2._3, p._2._1)).sortBy(_._3).collect.foreach(println)
-
-
-
-
 
 
 
@@ -283,65 +304,13 @@ object VcfForest extends SparkApp {
   val TestArray: Array[Double] = new Array(10)
   val RandArray: Array[Double] = new Array(10)
     
-  var a = 0
-  for( a <- 0 to 9){
-    val start = new Date().getTime
-    val trainingData =
-      if (a==0)       splits(1).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//0
-      else if (a==1)  splits(0).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//1
-      else if (a==2)  splits(0).union(splits(1)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//2
-      else if (a==3)  splits(0).union(splits(1)).union(splits(2)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//3
-      else if (a==4)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//3
-      else if (a==5)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//3
-      else if (a==6)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(7)).union(splits(8)).union(splits(9))//3
-      else if (a==7)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(8)).union(splits(9))//3
-      else if (a==8)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(9))//3
-      else            splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8))//4
-
-    val testData = splits(a)
-
-
-
-
-
-
-    val model = RandomForest.trainClassifier(
-                                             input=trainingData.map(_._4),
-                                             ClassificationStrategy,
-                                             numTrees = args(1).toInt,
-                                             featureSubsetStrategy = args(4),
-                                             seed)
-
-    val labelsAndPredictions = testData.map { point =>
-      val prediction = model.predict(point._4.features)
-      (point._1, point._2, point._4.label, prediction)
-    }.map(p => (p._1, p._2, p._3, p._4))
-
-
-    */
-
-    /**
-     * Stuff for regressor
-     */
-    /*
-    val model = RandomForest.trainRegressor(
-                                          input = trainingData.map(_._4),
-                                          RegressionStrategy,
-                                          numTrees = args(1).toInt,
-                                          featureSubsetStrategy = args(4),
-                                          seed)
-*/
-
-
-
-
 
     // Evaluate model on test instances and compute test error
 
     
     //labelsAndPredictions.collect().foreach(println)
 
-    /*
+
     println("Calculating metrics..")
     
     //// Adjusted Rand Index
