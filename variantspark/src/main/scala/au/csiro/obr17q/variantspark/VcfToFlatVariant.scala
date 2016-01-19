c6108b7b19930c06945dd398b523c53c762c5be5
O'Brien, Aidan (CCI, Acton) <Aidan.O'Brien@csiro.au>
2016-01-19 14:24:39 +1100

    Updated to work with my changes to vcfparser

--- variantspark/src/main/scala/au/csiro/obr17q/variantspark/VcfToFlatVariant.scala
+++ variantspark/src/main/scala/au/csiro/obr17q/variantspark/VcfToFlatVariant.scala
@@ -3,21 +3,20 @@ package au.csiro.obr17q.variantspark
 import org.apache.spark.rdd.RDD
 import au.csiro.obr17q.variantspark.model.FlatVariant
 import au.csiro.obr17q.variantspark.model.VcfParser
+import scala.io._
 
 object VcfToFlatVariant extends SparkApp {
   conf.setAppName("VCF cluster")
   def main(args:Array[String]) {
     if (args.length < 1) {
-        println("Usage: CsvClusterer <input-path> <output-path> <variant-cutoof>")
+        println("Usage: CsvClusterer <input-path> <output-path> <variant-cutof>")
     }
 
     val VcfFiles = args(0)
     val output = args(1)
     val VariantCutoff = args(2).toInt
     val IndividualMeta: RDD[IndividualMap] = null
-  
-    //val PopFiles = Source.fromFile("data/PGPParticipantSurvey-20150831064509.csv").getLines()
-    //val Populations = sc.parallelize(new PopulationMap(PopFiles, 1, ',', 0, 16 ).returnMap(IncludeGroups, ExcludeGroups))
+
     val vcfParser = new VcfParser(VcfFiles, VariantCutoff, IndividualMeta, sc)
     //val NoOfAlleles = vcfParser.variantCount
     val FilteredAlleles:RDD[FlatVariant] = vcfParser.individualVariants
