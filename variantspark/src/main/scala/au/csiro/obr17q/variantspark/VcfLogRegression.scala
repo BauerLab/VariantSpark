7c82e48442a1f40be6ab22ea3f01f219cf763e03
O'Brien, Aidan (CCI, Acton) <Aidan.O'Brien@csiro.au>
2016-02-02 14:54:16 +1100

    A few changes for compatability with my changes

--- variantspark/src/main/scala/au/csiro/obr17q/variantspark/VcfLogRegression.scala
+++ variantspark/src/main/scala/au/csiro/obr17q/variantspark/VcfLogRegression.scala
@@ -3,10 +3,11 @@ package au.csiro.obr17q.variantspark
 import java.util.Date
 
 import au.csiro.obr17q.variantspark.CommonFunctions._
-import au.csiro.obr17q.variantspark.model.VcfParser
+import au.csiro.obr17q.variantspark.model.ThousandGenomesVcfParser
 import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
-import org.apache.spark.mllib.linalg.{Vectors}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.mllib.regression.LabeledPoint
 
 import scala.io.Source
@@ -15,10 +16,10 @@ object VcfLogRegression extends SparkApp {
   conf.setAppName("VCF logistic regression")
   
   def main(args:Array[String]) {
+    val defaults = Array("data/data.vcf","0")
     val seed = 4363465
-    val VcfFiles = args(0)
-    val sample = args(1).toDouble
-    val VariantCutoff = args(4).toInt
+    val VcfFiles = if (args.length > 0) args(0) else defaults(0)
+    val VariantCutoff = if (args.length > 1) args(1).toInt else defaults(1).toInt
 
     /**
      * TCGA settings
@@ -31,14 +32,13 @@ object VcfLogRegression extends SparkApp {
     val IndividualMeta = sc.parallelize(new MetaDataParser(PopFiles, HeaderLines = 1, '\t', "", 0, 1 )(SexCol = 3).returnMap(IncludeGroups = Array("GBR","ASW", "CHB")))
     //val IndividualMeta = sc.parallelize(new MetaDataParser(PopFiles, HeaderLines = 1, '\t', "", 0, 2 )(SexCol = 3).returnMap())
 
-    
-    val vcfObject = new VcfParser(args(0), VariantCutoff, IndividualMeta, sc)
-
+    val vcfObject = new ThousandGenomesVcfParser(VcfFiles, VariantCutoff, IndividualMeta, sc, sqlContext)
     val NoOfAlleles = vcfObject.variantCount
 
     val FilteredAlleles = vcfObject.individualTuples
 
-
+    val data = vcfObject.data
+    /*
     val IndividualVariants = FilteredAlleles
     .groupByKey
     //.map(p => (p._1.split('_')(0).substring(0,12), (p._1.split('_')(1), p._2))) // TCGA data
@@ -61,91 +61,88 @@ object VcfLogRegression extends SparkApp {
 
     //.filter(_._2 == "NORMAL")
     .cache
+     */
 
 
 
 
-
-    println(IndividualVariants.count() + " individuals")
+    println(data.count() + " individuals")
     println("with " + NoOfAlleles + " alleles")
 
 
 
-  val TestArray: Array[Double] = new Array(10)
-  val RandArray: Array[Double] = new Array(10)
-  val splits = IndividualVariants.randomSplit(Array(0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1))
-  var a = 0
-  for( a <- 0 to 9){
-    val start = new Date().getTime
-    val trainingData =
-      if (a==0)       splits(1).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//0
-      else if (a==1)  splits(0).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//1
-      else if (a==2)  splits(0).union(splits(1)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//2
-      else if (a==3)  splits(0).union(splits(1)).union(splits(2)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//3
-      else if (a==4)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//3
-      else if (a==5)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(6)).union(splits(7)).union(splits(8)).union(splits(9))//3
-      else if (a==6)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(7)).union(splits(8)).union(splits(9))//3
-      else if (a==7)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(8)).union(splits(9))//3
-      else if (a==8)  splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(9))//3
-      else            splits(0).union(splits(1)).union(splits(2)).union(splits(3)).union(splits(4)).union(splits(5)).union(splits(6)).union(splits(7)).union(splits(8))//4
+    val TestArray: Array[Double] = new Array(10)
+    val RandArray: Array[Double] = new Array(10)
 
-    val testData = splits(a)
 
-    //val splits = SparseVariants.randomSplit(Array(0.6, 0.4), seed)
-    //val (trainingData, testData) = (splits(0), splits(1))
-  
-    val model = new LogisticRegressionWithLBFGS()
-      .setNumClasses(4)
-      .run(trainingData.map(_._4))
-   
-
-    val labelsAndPredictions = testData.map { point =>
-      val prediction = model.predict(point._4.features)
-      (point._1, point._2, point._4.label, prediction)
-    }.map(p => (p._1, p._2, p._3, p._4))
+    val Array(trainingData, testData) = data.randomSplit(Array(0.8, 0.2))
 
 
 
-    println("Calculating metrics..")
-    val metrics = new MulticlassMetrics(labelsAndPredictions.map(p => (p._3, p._4)))
-    val precision = Array(metrics.precision(0), metrics.precision(1), metrics.precision(2))
-    val recall = Array(metrics.recall(0), metrics.recall(1), metrics.recall(2))
-    
-    
-    
+    val lr = new LogisticRegression()
+
+
+    lr.setMaxIter(10)
+      .setRegParam(0.01)
+
+    val model1 = lr.fit(trainingData)
+
+
+    model1.transform(testData)
+
+
+
+    //val model = new LogisticRegressionWithLBFGS()
+    //  .setNumClasses(4)
+    // .run(trainingData.map(_._4))
+
+
+    //val labelsAndPredictions = testData.map { point =>
+    //  val prediction = model.predict(point._4.features)
+    //  (point._1, point._2, point._4.label, prediction)
+    //}.map(p => (p._1, p._2, p._3, p._4))
+
+
+
+    //println("Calculating metrics..")
+    //val metrics = new MulticlassMetrics(labelsAndPredictions.map(p => (p._3, p._4)))
+    //val precision = Array(metrics.precision(0), metrics.precision(1), metrics.precision(2))
+    //val recall = Array(metrics.recall(0), metrics.recall(1), metrics.recall(2))
+
+
+
     //val metrics = new MulticlassMetrics(labelsAndPredictions.map(p => (p._3, p._4)))
     //val precision = Array(metrics.precision(0), metrics.precision(1), metrics.precision(2), metrics.precision(3))
     //val recall = Array(metrics.recall(0), metrics.recall(1), metrics.recall(2), metrics.recall(3))
-    
-    
-    println("GBR, ASW, CHB")
-    println(metrics.confusionMatrix.toString())
-
-    println("GBR - precision:" + precision(0) + " recall:" + recall(0))
-    println("ASW - precision:" + precision(1) + " recall:" + recall(1))
-    println("CHB - precision:" + precision(2) + " recall:" + recall(2))
 
 
-    val resultArray: Array[(Double, Double)] = labelsAndPredictions.map(p => (p._3, p._4)).collect
-    val clustered = "[%s]".format(resultArray.map(_._1.toString()).reduceLeft(_+","+_))
-    val expected = "[%s]".format(resultArray.map(_._2.toString()).reduceLeft(_+","+_))
-    val adjustedRandIndex = GetRandIndex(clustered, expected)
-    println("Adjusted Rand Index = " + adjustedRandIndex)
-    
-    
-    val errors = labelsAndPredictions.filter(r => r._3 != r._4).count.toDouble
-    val total = testData.count()
-    val testErr = errors / total
-    
-    TestArray(a) = testErr
-    RandArray(a) = adjustedRandIndex.toDouble
-    val end = new Date().getTime
-    println("Job took "+(end-start)/1000 + " seconds")
-  }
-  
-    println("Test errors: " + TestArray.mkString(", "))
-    println("Adjusted Rand Indices: " + RandArray.mkString(", "))
-    
+    //println("GBR, ASW, CHB")
+    //println(metrics.confusionMatrix.toString())
+
+    //println("GBR - precision:" + precision(0) + " recall:" + recall(0))
+    //println("ASW - precision:" + precision(1) + " recall:" + recall(1))
+    //println("CHB - precision:" + precision(2) + " recall:" + recall(2))
+
+
+    //val resultArray: Array[(Double, Double)] = labelsAndPredictions.map(p => (p._3, p._4)).collect
+    //val clustered = "[%s]".format(resultArray.map(_._1.toString()).reduceLeft(_+","+_))
+    //val expected = "[%s]".format(resultArray.map(_._2.toString()).reduceLeft(_+","+_))
+    //val adjustedRandIndex = GetRandIndex(clustered, expected)
+    //println("Adjusted Rand Index = " + adjustedRandIndex)
+
+
+    //val errors = labelsAndPredictions.filter(r => r._3 != r._4).count.toDouble
+    //val total = testData.count()
+    //val testErr = errors / total
+
+    //TestArray(a) = testErr
+    //RandArray(a) = adjustedRandIndex.toDouble
+    //val end = new Date().getTime
+    //println("Job took "+(end-start)/1000 + " seconds")
+
+
+    //println("Test errors: " + TestArray.mkString(", "))
+    //println("Adjusted Rand Indices: " + RandArray.mkString(", "))
   }
   
 
