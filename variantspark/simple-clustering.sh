681c07ac3ab3dd3f892f1dec8acedd8208a5af27
aydun1 <ayduno@gmail.com>
2016-08-09 16:39:25 +1000

    Updated run scripts

---
+++ variantspark/simple-clustering.sh
@@ -0,0 +1,14 @@
+## Define the following 2 parameters
+input_file="/phase1/ALL.chr22.SHAPEIT2_integrated_phase1_v3.20101123.snps_indels_svs.genotypes.all.vcf"
+clusters=5
+
+
+spark-submit --class au.csiro.obr17q.variantspark.VcfClustering \
+      --master yarn-client \
+      --num-executors 32 \
+      --executor-memory 2G \
+      --executor-cores 1 \
+      --driver-memory 4G \
+      target/variantspark-0.0.1-SNAPSHOT.jar \
+      $input_file \
+      $clusters
