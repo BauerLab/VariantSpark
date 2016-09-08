075659832857b0882b18ebdd8df2c38ff3ba4945
aydun1 <ayduno@gmail.com>
2016-09-08 17:11:14 +1000

    Update for example file

--- variantspark/simple-clustering.sh
+++ variantspark/simple-clustering.sh
@@ -1,6 +1,6 @@
 ## Define the following 2 parameters
-input_file="/phase1/ALL.chr22.SHAPEIT2_integrated_phase1_v3.20101123.snps_indels_svs.genotypes.all.vcf"
-clusters=5
+input_file="ALL.chr22.SHAPEIT2_integrated_phase1_v3.20101123.snps_indels_svs.genotypes.all.vcf"
+clusters=4
 
 
 spark-submit --class au.csiro.obr17q.variantspark.VcfClustering \
